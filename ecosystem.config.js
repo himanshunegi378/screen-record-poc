203596574e7adbbc51415a26fafb11b15e88a9b9
Himanshu <himanshu.negi@samta.ai>
2026-06-05 14:49:59 +0530

    env chenage

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       ],
       env: {
         NODE_ENV: 'development',
-        PORT: 8000
+        PORT: 8089
       },
       env_production: {
         NODE_ENV: 'production',
